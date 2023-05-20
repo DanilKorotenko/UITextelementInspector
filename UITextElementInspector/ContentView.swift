b93dd465e29fed0fa1ebc2d21e10acf5e4b6dedb
Danil Korotenko <danil.korotenko@gmail.com>
2023-05-20 23:06:25 +0300

    Initial Commit

---
+++ UITextElementInspector/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UITextElementInspector
+//
+//  Created by Danil Korotenko on 5/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UITextElementInspector/UITextElementInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITextElementInspectorApp.swift
+//  UITextElementInspector
+//
+//  Created by Danil Korotenko on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct UITextElementInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
